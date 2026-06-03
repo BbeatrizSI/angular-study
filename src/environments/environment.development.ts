207ed52bd852cd8c1f4903f8ce84344f51d37725
Barbara Sacristan Ibañez <barbara.sacristan@keifitech.com>
2026-06-03 13:45:19 +0200

    update code

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  // https://developers.giphy.com/dashboard/
+  giphyApiKey: 'orbwGkq0WWcHaWLWcW8H02dSerywpipt',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
